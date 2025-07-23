31e5670d3dedc9f8e46eb26329c2e5e003ac8f34
jarno3636 <106784663+jarno3636@users.noreply.github.com>
2025-07-23 18:49:31 -0500

    Update assets.js

--- game/assets.js
+++ game/assets.js
@@ -1,19 +1,40 @@
-import k from "./kaplay"
+// Load Frog Sprites
+loadSprite("frog_blue", "assets/images/6BC994FD-53FC-48AF-BEDE-F72C3DA6421E.png");
+loadSprite("frog_green", "assets/images/frog_sprite_green.png");
+loadSprite("frog_red", "assets/images/frog_sprite_red.png");
+// Add more frog colors as you create them
 
-import jump from "./assets/sounds/jump.ogg"
-import rocket from "./assets/sounds/rocket.ogg"
-import gameOver from "./assets/sounds/game_over.ogg"
-import explosion1 from "./assets/sounds/explosion_1.ogg"
-import explosion2 from "./assets/sounds/explosion_2.ogg"
-import rocketAvailable from "./assets/sounds/rocket_available.ogg"
+// Lily Pads
+loadSprite("lily_pad", "assets/images/A_2D_digital_illustration_of_a_lily_pad_showcases_.png");
+loadSprite("lily_pad_cracked", "assets/images/A_2D_digital_illustration_features_a_cracked_lily_.png");
+loadSprite("lily_pad_single", "assets/images/A_2D_digital_illustration_features_a_single_lily_p.png");
 
-import kitchenSink from "./assets/fonts/kitchen_sink.ttf"
+// Collectibles
+loadSprite("collectible_triangle", "assets/images/A_pixel_art_digital_image_features_a_red_equilater.png");
+loadSprite("collectible_swirl", "assets/images/A_2D_digital_illustration_features_a_spiral_symbol.png");
+loadSprite("collectible_leaf", "assets/images/A_pixel_art_image_depicts_a_single_green_leaf_isol.png");
 
-k.loadFont("kitchen_sink", kitchenSink)
+// Lotus / Powerups
+loadSprite("lotus", "assets/images/A_pixel_art_sprite_sheet_of_six_identical_lotus_fl.png");
 
-k.loadSound('jump', jump)
-k.loadSound('rocket', rocket)
-k.loadSound('game_over', gameOver)
-k.loadSound('explosion_2', explosion2)
-k.loadSound('explosion_1', explosion1)
-k.loadSound('rocket_available', rocketAvailable)
+// Accessories
+loadSprite("accessories", "assets/images/A_pixel_art_sprite_sheet_titled_\"frog_accessories_.png");
+
+// Badges / Achievements
+loadSprite("badge_5", "assets/images/A_2D_digital_illustration_displays_five_achievemen.png");
+loadSprite("badge_10", "assets/images/A_collection_of_ten_digital_badge_illustrations_is.png");
+
+// Menus / UI
+loadSprite("main_menu", "assets/images/A_2D_digital_illustration_serves_as_the_main_menu_.png");
+loadSprite("home_ui", "assets/images/A_2D_digital_illustration_of_a_mobile_game_interfa.png");
+loadSprite("end_game", "assets/images/A_2D_digital_illustration_of_an_end-of-game_screen.png");
+loadSprite("ui_overlay", "assets/images/A_2D_digital_graphic_displays_a_game_user_interfac.png");
+
+// Lore / Scrolls
+loadSprite("lore_scroll", "assets/images/A_2D_digital_illustration_features_an_ancient_parc.png");
+
+// Game Logo
+loadSprite("logo", "assets/images/A_pixel_art_logo_for_the_video_game_\"Toad_Jumper\"_.png");
+
+// Background / Scenes
+loadSprite("background", "assets/images/32F896AD-ACFB-472A-B83A-82906F11ECAA.png");
